6fd36c078c3ad5794d74f2939e024d4f4a9f7a76
13083361602 <3265868153@qq.com>
2026-04-16 10:32:41 +0800

    初始化数据库连接池

--- transfer_server/transfer_server.cpp
+++ transfer_server/transfer_server.cpp
@@ -1,15 +1,18 @@
 #include "RPCServer.h"
 #include "TransferServiceImpl.h"
 #include <mymuduo/Log/Logger.h>
+#include <mymuduo/db/ConnectionPool.h>
 
 int main(int argc, char** argv)
 {
     // 假设你的内网环境，TransferServer 坚守在 8082 端口
     std::string ip = "127.0.0.1";
     uint16_t port = 8082;
-
     std::cout << "[INFO] === OmniBox TransferServer is starting on " << ip << ":" << port << " ===" << std::endl;
-
+    
+    // 初始化数据库连接池
+    ConnectionPool::Instance().Init("127.0.0.1", "root", "123456", "omni_box");
+    
     // 初始化你的 RPC 服务器 (基于 MyMuduo)
     RPCServer transfer_rpc_server(ip, port);
 
